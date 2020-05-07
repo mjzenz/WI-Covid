085dacb1e886d4a35d9e49a53cd3c49e3ffe289c
Mike Zenz <mail@mjzenz.org>
2020-05-06 22:52:34 -0500

    Fixed data read in from DHS after thier data change.

--- WI_COVID_DATA_PROCESS.R
+++ WI_COVID_DATA_PROCESS.R
@@ -11,9 +11,10 @@ library(lubridate)
 DATA_REQUEST <- httr::GET("https://opendata.arcgis.com/datasets/b913e9591eae4912b33dc5b4e88646c5_10.geojson")
 WI_COVID_DATA <-  fromJSON(rawToChar(DATA_REQUEST$content))
 WI_COVID_DATA <- WI_COVID_DATA[[c(4,2)]]
-WI_COVID_DATA <- WI_COVID_DATA[,2:7]
-WI_COVID_DATA$Date <- .POSIXct(WI_COVID_DATA$LoadDttm/1000)
-WI_COVID_DATA$Date <- lubridate::date(WI_COVID_DATA$Date)
+WI_COVID_DATA <- WI_COVID_DATA[,1:7]
+#LoadDttm changed in the data, so I removed this line.
+#WI_COVID_DATA$Date <- .POSIXct(WI_COVID_DATA$LoadDttm/1000)
+WI_COVID_DATA$Date <- lubridate::date(WI_COVID_DATA$LoadDttm)
 
 #READ IN A FILE WITH COUNTY POPULATION VALUES AND REGION FROM DHS.  NOTE THAT THESE ARE 2018 VALUES.
 WI_POP <- read_csv("WI_POP.csv")
@@ -23,20 +24,39 @@ WI_POP <- merge(x = WI_POP, y = REGION_POP, by = "REGION")
 TOP_6_POP_COUNTIES <- WI_POP[order(-WI_POP$POPULATION),c("COUNTY")]
 TOP_6_POP_COUNTIES <- TOP_6_POP_COUNTIES[1:6]
 
-COUNTY_VALUES <- WI_COVID_DATA[which(WI_COVID_DATA$GEO == "County"), c(1:7)]
+COUNTY_VALUES <- WI_COVID_DATA[which(WI_COVID_DATA$GEO == "County"), c(1:8)]
 COUNTY_VALUES$COUNTY <- str_to_upper(COUNTY_VALUES$NAME)
 COUNTY_VALUES <- merge(x = COUNTY_VALUES, y = WI_POP, by = "COUNTY")
 COUNTY_VALUES$COUNTY_POSITIVE_PER_THOUS <- COUNTY_VALUES$POSITIVE / (COUNTY_VALUES$POPULATION/1000) 
 COUNTY_VALUES$TOTAL_TESTS <- COUNTY_VALUES$NEGATIVE + COUNTY_VALUES$POSITIVE
 COUNTY_VALUES$PERCENT_POS <- COUNTY_VALUES$POSITIVE / COUNTY_VALUES$TOTAL_TESTS
+
+#CALCULATE DAILY CHANGES BY COUNTY
+COUNTY_VALUES <- COUNTY_VALUES[order(COUNTY_VALUES$COUNTY, COUNTY_VALUES$Date ),]
+
+COUNTY_VALUES <- COUNTY_VALUES %>%
+                  group_by(COUNTY) %>%
+                  mutate(.,
+                         CHANGE_POS = POSITIVE - lag(POSITIVE)
+                         )
+
+
 COUNTY_VALUES_TOP_6_COUNTIES <- COUNTY_VALUES[which(COUNTY_VALUES$COUNTY %in% TOP_6_POP_COUNTIES),]
+
+
+
+
+
+
+
 
 REGION_VALUES <- COUNTY_VALUES %>%
                   group_by(Date, REGION) %>%
                   summarize(., 
                             NEGATIVE = sum(NEGATIVE),
                             POSITIVE = sum(POSITIVE),
-                            REG_POP = sum(POPULATION)
+                            CHANGE_POS = sum(CHANGE_POS),
+                            REG_POP = sum(POPULATION),
                   ) %>%
                   mutate(.,
                          TOTAL_TESTS = NEGATIVE + POSITIVE,
