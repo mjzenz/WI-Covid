1e0b8544b837ddce53c5ae45087e8d50bc60ceea
MICHAEL J ZENZ <zenz@wisc.edu>
2021-04-16 19:57:57 -0500

    Updated api address.

--- WI_COVID_DATA_PROCESS.R
+++ WI_COVID_DATA_PROCESS.R
@@ -15,7 +15,8 @@ MIN_DATE_INTERVAL <- -3
 #READS IN DATA FROM WI DHHS API
 #County Filter Stopped Working, so removed it.
 #DATA.REQUEST  <- httr::GET("https://opendata.arcgis.com/datasets/b913e9591eae4912b33dc5b4e88646c5_10.geojson?where=GEO%20%3D%20'County'")
-DATA.REQUEST  <- httr::GET("https://opendata.arcgis.com/datasets/5374188992374b318d3e2305216ee413_12.geojson")
+#DATA.REQUEST  <- httr::GET("https://opendata.arcgis.com/datasets/5374188992374b318d3e2305216ee413_12.geojson")
+DATA.REQUEST <- httr::GET("https://opendata.arcgis.com/datasets/0b7bac0afc464e7783474cb62272d9b8_12.geojson")
 
 print("Downloaded data from DHS")
 WI_COVID_DATA <-  fromJSON(rawToChar(DATA.REQUEST$content))
